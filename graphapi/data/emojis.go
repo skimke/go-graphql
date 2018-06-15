b449be0bd1743bc6951367157d59c63c62811c86
Stella Kim <stella.kim@icloud.com>
2018-06-14 20:29:58 -0400

    func for grabbing a random emoji unicode

--- graphapi/data/emojis.go
+++ graphapi/data/emojis.go
@@ -1,5 +1,17 @@
 package data
 
+import (
+	"math/rand"
+)
+
 type Emojis struct {
 	Text string
+}
+
+var emojisList = []string{"\U0001f487", "\U0001f63b", "\U0001f36f", "\U0001f428", "\U0001f34b", "\U0001f913", "\U0001f30a", "\U0001f44c", "\U0001f989", "\U0001f6a2", "\U0001f916", "\U0001f64a", "\u2747\ufe0f", "\U0001f375", "\U0001f60b", "\U0001f680"}
+
+func getRandom() string {
+	n := rand.Intn(len(emojisList))
+
+	return emojisList[n]
 }
